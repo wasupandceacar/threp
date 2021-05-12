22dc646780a0c2237486beb0429fd95e1d2512fd
wasupandceacar <wasupandceacar@gmail.com>
2021-05-13 02:01:47 +0800

    workflow 测试 1

--- threp_example.py
+++ threp_example.py
@@ -2,7 +2,7 @@ from api import THReplay
 
 if __name__ == '__main__':
     # 载入一个replay文件，参数为路径
-    tr = THReplay('rep/th13_01.rpy')
+    tr = THReplay('rep_tst/th13_01.rpy')
 
     # 获取rep基本信息，包含机体，难度，通关情况，字符串
     # etc. Reimu A Normal All
@@ -69,4 +69,4 @@ if __name__ == '__main__':
     print(tr.getShift())
 
     #载入一个新的replay文件，参数为路径
-    #tr.reload_replay("th15_02.rpy")
+    tr.reload_replay('rep_tst/th13_07.rpy')

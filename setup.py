bc001afad994e0983a5ae626799c8c060745e745
wasupandceacar <wasupandceacar@gmail.com>
2021-05-25 03:54:44 +0800

    actions pypi 修改

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="threp",
+    version="2.0.0",
+    author="wasupandceacar",
+    author_email="wasupandceacar@gmail.com",
+    description="decode Touhou Shooting Game's replay files and get the infomation in it",
+    license="MIT",
+    long_description=open('README.rst', encoding='UTF-8').read(),
+    url="https://github.com/wasupandceacar/threp",
+    packages=['threp'],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Games/Entertainment",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords='touhou replay python',
+)
